ad6e18fae169adc13aefb419e49e178f64dec46f
Rojotiana Nomena <hei.nomena@gmail.com>
2025-07-01 17:50:10 +0300

    chore(wording): update word

--- script/script.js
+++ script/script.js
@@ -1,35 +1,132 @@
-window.addEventListener("DOMContentLoaded", () => {
-
-    generator.addMenu()
-    
-})
+window.addEventListener('DOMContentLoaded', () => {
+  generator.addMenu();
+});
 
 // Variable contenant le mot precedant pour eviter une repetition de mot
-let oldWord = ''
+let oldWord = '';
 
 // Tableau contenant les mots
 const words = [
-    ['bug', 'var', 'let', 'for', 'map', 'set', 'css', 'alt', 'php', 'web'],
-    ['html', 'loop', 'show', 'drop', 'hide', 'push', 'date', 'data', 'char', 'java'],
-    ['const', 'split', 'error', 'width', 'class', 'click', 'enter', 'event', 'array', 'index'],
-    ['height', 'margin', 'python', 'cookie', 'script', 'append', 'object', 'loaded', 'window', 'system'],
-    ['listner', 'windows', 'control', 'console', 'content', 'comment', 'default', 'prevent', 'monitor', 'element'],
-    ['function', 'dataview', 'computer', 'keyboard', 'language', 'keyframe', 'document', 'external', 'children', 'contains'],
-    ['innerhtml', 'innertext', 'lowercase', 'uppercase', 'classname', 'bootstrap', 'animation', 'cryptokey', 'clonenode', 'lastchild'],
-    ['javascript', 'closeevent', 'childnodes', 'firstchild', 'ondblclick', 'onkeypress', 'fullscreen', 'parentnode', 'parsefloat', 'settimeout'],
-    ['application', 'getattribute', 'setattribute', 'contenttype', 'createrange', 'removechild', 'defaultview', 'getrootnode', 'onloadstart', 'onmousedown'],
-    ['contentloaded', 'onloadeddata', 'insertbefore', 'lastmodified', 'onmouseenter', 'setinterval', 'syntaxerror', 'getelementbyid', 'programming', 'congratulation'],
-]
-let WORD = ''
+  ['bug', 'api', 'dom', 'sdk', 'sql', 'nan', 'css', 'ide', 'git', 'cli'],
+  [
+    'node',
+    'code',
+    'java',
+    'html',
+    'json',
+    'hook',
+    'http',
+    'type',
+    'test',
+    'bool',
+  ],
+  [
+    'array',
+    'linux',
+    'merge',
+    'token',
+    'error',
+    'debug',
+    'react',
+    'props',
+    'cloud',
+    'async',
+  ],
+  [
+    'commit',
+    'gitlab',
+    'import',
+    'lambda',
+    'script',
+    'branch',
+    'object',
+    'syntax',
+    'return',
+    'system',
+  ],
+  [
+    'backend',
+    'control',
+    'console',
+    'content',
+    'comment',
+    'default',
+    'virtual',
+    'request',
+    'promise',
+    'pointer',
+  ],
+  [
+    'function',
+    'frontend',
+    'devtools',
+    'keyboard',
+    'language',
+    'keyframe',
+    'document',
+    'debugger',
+    'database',
+    'contains',
+  ],
+  [
+    'framework',
+    'websocket',
+    'lowercase',
+    'uppercase',
+    'classname',
+    'bootstrap',
+    'animation',
+    'algorithm',
+    'exception',
+    'singleton',
+  ],
+  [
+    'javascript',
+    'middleware',
+    'deployment',
+    'asyncawait',
+    'scrollview',
+    'onkeypress',
+    'fullscreen',
+    'navigation',
+    'parsefloat',
+    'settimeout',
+  ],
+  [
+    'application',
+    'getattribute',
+    'setattribute',
+    'contenttype',
+    'refactoring',
+    'removechild',
+    'defaultview',
+    'environment',
+    'onloadstart',
+    'onmousedown',
+  ],
+  [
+    'Authentication',
+    'Authorization',
+    'insertbefore',
+    'troubleshoot',
+    'onmouseenter',
+    'setinterval',
+    'syntaxerror',
+    'getelementbyid',
+    'programming',
+    'congratulation',
+  ],
+];
+let WORD = '';
 
 // objet contenant les meilleurs scores
 let HIGH = {
-    niveau: 0,
-    score: 0,
-}
+  niveau: 0,
+  score: 0,
+};
 
 // variable utile pour le fonctionnement du jeu
-let essaye = 5
-let score = 0
-let niveau = 1
-let variance = 1
+let essaye = 5;
+let score = 0;
+let niveau = 1;
+let variance = 1;
